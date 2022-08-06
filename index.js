0dba10f571ad4c07a4adafe2f6e68f4bfdc82230
Ria Legaspi <riaveluz@gmail.com>
2022-08-06 14:08:52 -0700

    commit tree d736cf4bf0ea92135872f03615e2f79c421c5c60

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener( "DOMContentLoaded", function () {
+    newDOM()
+  } );
+  
+  
+  function newDOM() {
+    document.getElementById( "text" )
+      .innerHTML = "This is really cool!";
+  }
